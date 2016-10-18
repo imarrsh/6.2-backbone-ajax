dda256de3b867249e18ac13802a4977c6b8d55b9
Marshall Thompson <marshallthmomp@gmail.com>
2016-10-18 14:39:30 -0400

    on event firing for each new model

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,8 +1,9 @@
 var jQuery = require('jquery');
-var Backbone = require('ackbone');
+var Backbone = require('backbone');
 
 var models = require('./models/posts');
 
+//DOM ready
 (function($){
   // INITIAL Requirements:
   // get the submit/loading button
@@ -10,7 +11,28 @@ var models = require('./models/posts');
   // disable the button, change button text to 'Loading...'
   // when ajax is done, change the text back to the submit
 
-  $('#fetch-posts');
+  // instantiate a new collection
+  var allPosts = new models.PostCollection();
+
+  $('#fetch-posts') // get the button
+    .on('click', function(){
+      var $this = $(this);
+      // backbone ajax method (push to call stack)
+      allPosts.fetch().then(function(){
+        // make available again
+        $this.text('Submit');
+        $this.prop('disabled', false);
+      });
+      // disable the button while we wait
+      $this.text('Loading...');
+      $this.prop('disabled', true);
+
+      // backbone event listener/handler
+      allPosts.on('add', function(){
+        console.log('emitting add event');
+      });
+
+    });
 
 
 }(jQuery));
--- app/scripts/models/posts.js
+++ app/scripts/models/posts.js
@@ -2,12 +2,30 @@ var $ = require('jquery');
 var Backbone = require('backbone');
 
 // set up model container
-var Post = Backbone.Model.extend();
+var Post = Backbone.Model.extend({
+  defaults :{
+    "htmlText" : "Submit"
+  },
+  isLoading: function(){
+
+  }
+});
 
 var PostCollection = Backbone.Collection.extend({
   model: Post,
-  url: 'http://tiny-lasagna-server.herokuapp.com'
+  url: 'http://tiny-lasagna-server.herokuapp.com/collections/posts'
 });
+
+// // instantiate a new collection
+// var allPosts = new PostCollection();
+//
+// // backbone ajax method
+// allPosts.fetch();
+//
+// // backbone event listener/handler
+// allPosts.on('add', function(){
+//   console.log('emitting add event');
+// });
 
 module.exports = {
   Post : Post,
